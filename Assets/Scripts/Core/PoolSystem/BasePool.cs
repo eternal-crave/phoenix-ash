257ba9bdd67958377e04655f0ec487b56cb74b4f
David Khudaverydan <134926510+eternal-crave@users.noreply.github.com>
2022-10-03 01:52:31 +0400

    Added missing files

---
+++ Assets/Scripts/Core/PoolSystem/BasePool.cs
@@ -0,0 +1,18 @@
+﻿using Core.PoolSystem;
+using UnityEngine;
+
+namespace Core.PoolSystem
+{
+    public class BasePool<T> : IPool<T> where T : IPoolObject
+    {
+        public BasePool()
+        {
+            Debug.Log("THIS FUCKIN POOL HAS BEEN CREATED !!!!!!");
+        }
+
+        public override T GetInstance()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/PoolSystem/IPool.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.PoolSystem
+{
+    public abstract class IPool<T> where T : IPoolObject
+    {
+        private Queue<T> pool;
+        private List<T> active;
+        public abstract T GetInstance();
+    }
+}

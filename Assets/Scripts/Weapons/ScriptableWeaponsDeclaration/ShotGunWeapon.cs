fbfb5e6caf11e1653242c1e6167834f4bb76b327
David Khudaverydan <134926510+eternal-crave@users.noreply.github.com>
2022-10-25 13:01:29 +0400

    Fixed shotgun weapon... changed its scriptable object name

--- Assets/Scripts/Weapons/ScriptableWeaponsDeclaration/ShotGunWeapon.cs
+++ Assets/Scripts/Weapons/ScriptableWeaponsDeclaration/ShotGunWeapon.cs
@@ -8,7 +8,6 @@ namespace Weapons
     [CreateAssetMenu(fileName = "SemiCircleWeapon", menuName = "Weapons/SemiCircleWeapon")]
     public class ShotGunWeapon : Weapon
     {
-        [SerializeField] private float offset;
         [SerializeField] private float step;
         [SerializeField] private int amountOfBullets;
         public override void Shoot(Vector3 origin, Quaternion rotation, Vector3 direction)
@@ -18,11 +17,13 @@ namespace Weapons
             {
                 return;
             }
-
-
-            for(int i = -(amountOfBullets/2); i < amountOfBullets/2; i++)
+            
+            float dist = step * amountOfBullets;
+            float offset = amountOfBullets % 2 == 1 ? 0 : (dist / (amountOfBullets - 1))/2;
+            float amountOfInterations = amountOfBullets % 2 == 1 ? amountOfBullets/2+1 : amountOfBullets/2;
+            for (int i = -amountOfBullets/2; i < amountOfInterations; i++)
             {
-                float localStep = i * step;
+                float localStep = i*(dist / (amountOfBullets - 1)) + offset;
                 Bullet bullet = GetAmmo();
                 bullet.Activate();
                 bullet.transform.position = new Vector3(origin.x + localStep, origin.y, 0);
